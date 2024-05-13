6aedadd5de93d820731a25e1df5b18e5c9fdaf41
HarishKumarS <harishkumarsedu@gmail.com>
2024-05-13 16:27:48 +0200

    version updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('./README.md', 'r') as file :
     
 setup(
     name='IvmInstruments',
-    version='0.2.2',
+    version='0.2.4',
     py_modules=['Instruments'],
     description=[ 'text/x-rst','text/markdown'],
     long_description=long_description,

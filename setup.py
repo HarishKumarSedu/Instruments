bd3d105825e50c3cef6dcf1b9797f83f93ef8c91
HarishKumarS <harishkumarsedu@gmail.com>
2024-05-13 14:07:55 +0200

    workflow updated

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
-
+with open('./README.md', 'r') as file :
+    long_description = file.read()
 
 setup(
     name='IvmInstruments',
     version='0.1.7',
+    long_description=long_description,
+    long_description_content_type='markdown/text'
     py_modules=['Instruments'],
     packages=find_packages(),
     include_dirs=['Instruments'],

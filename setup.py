64177268598fefc6af63b3fec82f8050c9d704fb
HarishKumarS <harishkumarsedu@gmail.com>
2024-05-13 15:44:41 +0200

    version updated

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('./README.md', 'r') as file :
+    long_description = file.read()
 setup(
     name='IvmInstruments',
     version='0.2.1',
     py_modules=['Instruments'],
     description=['text/x-rst', 'text/plain', 'text/markdown'],
-    long_description='testing',
+    long_description=long_description,
     packages=find_packages(),
     include_dirs=['Instruments'],
     

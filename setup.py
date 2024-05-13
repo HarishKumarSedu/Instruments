52888455858ef4b15479439cc5c5ec630de551cb
HarishKumarS <harishkumarsedu@gmail.com>
2024-05-13 15:42:37 +0200

    version updated

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.2.1',
     py_modules=['Instruments'],
     description=['text/x-rst', 'text/plain', 'text/markdown'],
+    long_description='testing',
     packages=find_packages(),
     include_dirs=['Instruments'],
     
